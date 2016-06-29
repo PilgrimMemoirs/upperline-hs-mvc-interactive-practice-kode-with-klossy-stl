ba0321f44feafd2bc27d3d908548e9953abeb8d6
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-06-29 21:58:45 +0000

    class demo

--- application_controller.rb
+++ application_controller.rb
@@ -1,10 +1,44 @@
 require 'bundler'
 Bundler.require
+require_relative 'models/pig_latinize.rb'
 
 class MyApp < Sinatra::Base
 
   get '/' do
-    erb :index
+     erb :index
   end
 
-end
+  post '/fn' do
+    params[:first_name]
+  end
+
+  get '/pig_latin' do
+    erb :to_pig_latin
+  end
+
+  post '/piglatinize' do
+    to_pig_latin(params[:phrase]) + params[:first_name]
+  end
+
+  get '/kookies' do
+      erb :kookies
+  end
+
+  get '/greet/:time' do
+    time =  params[:time]
+    if time == "morning"
+      @greet = "Wake up!"
+    elsif time == "afternoon"
+      @greet =  "Nap time!"
+    elsif time == "night"
+      @greet = "GO TO BED!"
+    else
+      @greet = "I have no idea what you're talking about"
+    end
+    erb :greet
+  end
+
+
+
+
+end #ends sinatra class
